ba0def5e741f6e0c412d8b88ff51c70fb4528d1a
Thanakorn <bloodexec@gmail.com>
2026-06-25 15:33:04 +0700

    Add initial VSCode configuration and sample C program for DG111 course

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("สวัสดีครับ ผม [นายธนากร สุขโท้]\n");
+    printf("รหัสนักศึกษา: [692110145]\n");
+    printf("วิชา: DG111 Computer Programming\n");
+    return 0;
+}
